bfb30d5c9ad7296a0f48cb9ff16607bc3ac9fa66
Johnny-smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-09-16 23:03:00 -0500

    proper code

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,62 +16,60 @@ var enemyAttack = 12;
 // Alert users that they are starting the round
     //window.alert("Welcome to Robot Gladiators!");
 var fight = function(enemyName) {
-    //repate and execute as long as the enemy robot is alive
-    while(enemyHealth > 0) {
+  while (playerHealth > 0 && enemyHealth > 0) {
+    // ask user if they'd liked to fight or run
+    var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
-// Contiue? prompt
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT or 'SKIP to choose.");
-    console.log(promptFight);
-// If player choses to fight, then fight.
-if (promptFight === "fight" || promptFight === "FIGHT" ) {
-  //player attack
+    // if user picks "skip" confirm and then stop the loop
+    if (promptFight === "skip" || promptFight === "SKIP") {
+      // confirm user wants to skip
+      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+      // if yes (true), leave fight
+      if (confirmSkip) {
+        window.alert(playerName + ' has decided to skip this fight. Goodbye!');
+        // subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 10;
+        console.log("playerMoney", playerMoney)
+        break;
+      }
+    }
+
+    // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
-  //player attack log
-    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
-  // check enemy's health
-  if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died!");
-}
-  else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
-}
+    console.log(
+      playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
+    );
 
-  //enemy attack      
+    // check enemy's health
+    if (enemyHealth <= 0) {
+      window.alert(enemyName + ' has died!');
+
+      // award player money for winning
+      playerMoney = playerMoney + 20;
+
+      // leave while() loop since enemy is dead
+      break;
+    } else {
+      window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
+    }
+
+    // remove players's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
-  // enemy attack log
-    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health reamaining.");
+    console.log(
+      enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
+    );
 
-  // check player's health
-  if (playerHealth <= 0) {
-    window.alert(playerName + " has died!");
-} 
-  else {
-    window.alert(playerName + " still has " + playerHealth + " health left.");
-}
-
-//If player skips, then skip.
-} else if (promptFight === "skip" || promptFight === "SKIP") {
-  
-  //confirm skip
-  var confirmSkip = window.confirm("Confirm skip?");
-  console.log(window.confirm);
-    //if yes(ture), leave fight
-    if (confirmSkip) {
-      window.alert(playerName + " has chosen to skip the fight! Goodbye!");
-      // subtract money
-        playerMoney = playerMoney - 2;
+    // check player's health
+    if (playerHealth <= 0) {
+      window.alert(playerName + ' has died!');
+      // leave while() loop if player is dead
+      break;
+    } else {
+      window.alert(playerName + ' still has ' + playerHealth + ' health left.');
     }
-      // if no (false), ask question agian by running fight() agian
-        else {
-          fight ();
-        }
-    
-} else {
-  window.alert("You need to pick a valid option. Try agian!");
-}
-    }
-
-}
+  }  
+};
 
 for(var i = 0; i < enemyNames.length; i++) {
   //debugger;
